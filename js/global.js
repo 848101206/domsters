14ae4223cd44465998a158c5473414a627002deb
848101206 <1334823022@qq.com>
2021-03-31 23:02:58 +0800

    20210331 23:02

--- js/global.js
+++ js/global.js
@@ -287,5 +287,82 @@ function displayAbbreviations(){
   var article = docment.getElementsByTagName("article");
   if(article.length == 0) return false;
   article[0].appendChild(header);
-  article[0].appendChild(dlist);
+  article[0].appendChild(dlist)
+}
+
+
+//Contact
+
+function focusLabels(){
+  if (!document.getElementsByTagName) return false;
+  var labels = document.getElementsByTagName("label");
+  for (var i = 0; i<labels.kength; i++){
+    if(!labels[i].getAttribute("for")) continue;
+    labels[i].onclick = function(){
+      var id  = this.getAttribute("for");
+      if(!document.getElementsById(id)) return false;
+      var element = document.getElementsById(id);
+      element.focus();
+    }
+  }
+}
+
+function resetFields(whichform){
+  if(Modernizr.input.placeholder) return;
+  for( var i = 0; i<whichform.element.length; i++){
+    var element = whichform.elements[i];
+    if(!element.type =="submit")continue;
+    if(!element.getAttribute('placeholder')) continue;
+    element.onfocus = function(){
+      if(this.value == this.getAttribute('placeholder')){
+        this.value="";
+      }
+    }
+    element.onblur =function(){
+      if(this.value==""){
+        this.value = this.getAttribute('placeholder');
+      }
+    }
+    element.onblur();
+  }
+}
+
+function validateForm(whichform){
+  for(var i =0; i<whichform.elements.length;i++){
+    var element = whichform.elements[i];
+    if(element.getAttribute("required")=='required'){
+      if(!isFilled(element)){
+        alert("Please fill in the"+element.name+"field.");
+        return false;
+      }
+    }
+    if (element.getAttribute("type")=='email'){
+      if(!isEmail(element)){
+        alert("The"+element.name+"field must be a valid email address");
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function isFilled(fiels){
+  return (field.value.length > 1 && fiels.valeue != field.placeholder);
+}
+
+function isEmail(field){
+  return (field.value.indexof("@") != -1 && field.value.indexOf(".")!=-1);
+}
+
+function prepareForms() {
+  for(var i =0; i<document.forms.length;i++){
+    var thisform = document.forms[i];
+    resetFields(thisform);
+    thisform.onsubmit = function(){
+      if(!validateForm(this)) return false;
+      var article = document.getElementsByTagName('article')[0];
+      if(subminFormWithAjac(this,article)) return false;
+      return true;
+    }
+  }
 }
